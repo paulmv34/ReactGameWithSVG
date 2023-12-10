a33d4fd1b97f0cf89d5b0ac689d7b3b3aea72867
Pavel Vinogradov <disintegration777@yandex.ru>
2023-12-10 04:49:31 +0300

    fix: Убрал подключение неиспользуемых модулей

--- packages/client/src/pages/About/About.tsx
+++ packages/client/src/pages/About/About.tsx
@@ -1,5 +1,4 @@
 import { FC } from 'react'
-import { Link } from 'react-router-dom'
 import { ROUTES } from '../../routes/Routes'
 import ArrowBack from '../../components/ArrowBack/ArrowBack'
 import Header from '../../components/Header/Header'
