bf49c4dddeee65920ffe160b4b5d7d947d4b4a53
ZaoBaoo <yurkin.daniil96@gmail.com>
2023-12-12 22:16:59 +0600

    fix: отображение 500 и 404 ошибки; добавил возможность менять текст ошибки

--- packages/client/src/pages/Error/Error.tsx
+++ packages/client/src/pages/Error/Error.tsx
@@ -9,10 +9,11 @@ import { ErrorPageProps } from './types'
 
 const typesErrors: Record<string, string> = {
   '500': 'Что-то пошло не так',
+  '404': 'Страница не найдена',
 }
 
 const Error: FC<ErrorPageProps> = (props) => {
-  const { codeError } = props
+  const { codeError, textError } = props
 
   return (
     <main>
@@ -20,7 +21,7 @@ const Error: FC<ErrorPageProps> = (props) => {
         <Header />
         <div className={clsx('wrapper', styles['wrapper-error'])}>
           <h1 className={styles['code-error']}>{codeError}</h1>
-          <p className={styles['description-error']}>{typesErrors[codeError] || 'Неизвестная ошибка'}</p>
+          <p className={styles['description-error']}>{textError || typesErrors[codeError]}</p>
           <Link className={styles['link-error']} to={ROUTES.MAIN}>
             На главную
           </Link>
--- packages/client/src/pages/Error/types.ts
+++ packages/client/src/pages/Error/types.ts
@@ -1,3 +1,4 @@
 export interface ErrorPageProps {
   codeError: number | string
+  textError?: string
 }
