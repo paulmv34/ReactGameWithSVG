1afc749ad10ee9c84f78d91c3e505358d20ff3d4
Dastan Zhamekeshev <dastan132000@mail.ru>
2023-12-12 21:39:54 +0600

    feature(NON-21): сверстал страницу регистрации

---
+++ packages/client/src/pages/SignUp/SignUp.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react'
+import clsx from 'clsx'
+import styles from '@/pages/SignIn/SignIn.module.scss'
+import BackLink from '@/components/BackLink/BackLink'
+import { ROUTES } from '@/types/types'
+import SignUpForm from '@/modules/SignUpForm/SignUpForm'
+
+const SignUp: FC = () => {
+  return (
+    <section className={clsx(styles.page, 'page')}>
+      <div className="wrapper wrapper-backlink">
+        <BackLink to={ROUTES.MAIN} left="13%" />
+        <h2 className={clsx(styles.title, 'page-title')}>Регистрация</h2>
+        <SignUpForm />
+      </div>
+    </section>
+  )
+}
+
+export default SignUp
