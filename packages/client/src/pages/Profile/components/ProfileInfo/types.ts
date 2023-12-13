88e8da68c97f98267c357900ee8ed14f5d56a308
Radik152 <rezyabkinradik@gmail.com>
2023-12-13 15:34:44 +0300

    commit

--- packages/client/src/pages/Profile/components/ProfileInfo/types.ts
+++ packages/client/src/pages/Profile/components/ProfileInfo/types.ts
@@ -1,5 +1,10 @@
-export interface IProfile {
-  login: string
+export type TUserData = {
+  // id?: number
+  display_name: string
   email: string
+  first_name: string
+  login: string
   phone: string
+  second_name: string
+  // avatar?: string | null
 }
