d341e19ddc847a7ad3d88ea61990cad81b21d8a8
Radik152 <rezyabkinradik@gmail.com>
2023-12-25 15:38:38 +0300

    Правка по sw

--- packages/client/public/serviceWorker.js
+++ packages/client/public/serviceWorker.js
@@ -1,4 +1,20 @@
-import { ROUTES } from './src/types/types.ts';
+const ROUTES = {
+  ABOUT: '/about',
+  ERROR_500: '/500',
+  FORUM: '/forum',
+  GAME: '/game',
+  GAME_START: '/game/start',
+  LEADERBOARD: '/leaderboard',
+  LOGIN: '/sign-in',
+  MAIN: '/',
+  PROFILE: '/profile',
+  PROFILE_AVATAR: '/profile/changeAvatar',
+  PROFILE_PASSWORD: '/profile/changePassword',
+  REGISTRATION: '/sign-up',
+  TOPIC_FORUM: '/forum/*',
+  TOPIC_NEW: '/forum/new',
+};
+
 
 const CACHE_NAME = 'cache-v1'
 
