52bcc2b130a645fa5ff563dcc01f6d032c80be5f
Dastan Zhamekeshev <dastan132000@mail.ru>
2023-12-30 12:24:50 +0600

    chore: добавил поясняющий комментарий к логике ProtectedRoute

--- packages/client/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ packages/client/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -9,9 +9,12 @@ const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   const { isLoggedIn } = useAuth()
   const location = useLocation()
 
+  // Если пользователь не авторизован и пытается получить доступ к защищённой странице, то мы перенаправляем его на страницу авторизации
   if (isLoggedIn === false) {
     return <Navigate to={ROUTES.LOGIN} replace state={{ path: location.pathname }} />
-  } else if (isLoggedIn === null) {
+  }
+  // значение null нужно для того, чтобы не получилось редиректа на страницу авторизации для УЖЕ авторизованного пользователя
+  else if (isLoggedIn === null) {
     return null
   }
 
