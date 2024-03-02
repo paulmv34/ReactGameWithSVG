e5ad1a2cc8a7e49f5c0bcfa770bc827f5b9bc3e0
Pavel Vinogradov <paulmv34@gmail.com>
2024-03-02 10:44:43 +0300

    fix(non-73): корректировки cors для корректной работы приложения на облаке

--- packages/server/middlewares/cors.ts
+++ packages/server/middlewares/cors.ts
@@ -1,10 +1,14 @@
 import cors from 'cors'
 
 export const corsMiddleware = () => {
-  const clientPort = Number(process.env.CLIENT_PORT) || 3000
+  const serverPort = Number(process.env.SERVER_PORT) || 3001
+  const API_HOST = typeof process !== 'undefined' ? process.env?.API_HOST + ':' + serverPort : ''
+  const origins = [`http://127.0.0.1:${serverPort}`, `http://localhost:${serverPort}`]
+  if (API_HOST !== '') origins.push(API_HOST)
+
   const corsOptions = {
     credentials: true,
-    origin: [`http://127.0.0.1:${clientPort}`, `http://localhost:${clientPort}`],
+    origin: origins,
     optionsSuccessStatus: 200,
   }
 
